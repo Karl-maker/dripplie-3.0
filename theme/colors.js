7d13769f490538b7d58b1032cba4e1e61a7337f4
karl-maker <karljohanbailey98@gmail.com>
2022-11-18 12:02:49 -0400

    Add Comment and Description
    Added to theme/colors.js file which was missing a description

--- theme/colors.js
+++ theme/colors.js
@@ -1,3 +1,11 @@
+/**
+ *
+ * @author Karl-Johan Bailey
+ * @created 18/11/2022
+ * @desc Edit colors for themes here for easy editing and design changes
+ *
+ */
+
 const light = {
   background: { main: "#dfe6e9" },
   text: { primary: "#2d3436" },
