4fb9206ede744f7fa91b75dd718eca56c5576091
karl-maker <karljohanbailey98@gmail.com>
2022-11-18 11:30:37 -0400

    Button Analytics Skeleton
    Skeleton to add to all buttons for early development. This is really to avoid going to every button to add this peice of code when Google Analytics is connected

---
+++ utils/analytics/index.js
@@ -0,0 +1,22 @@
+/**
+ *
+ * @author Karl-Johan Bailey
+ * @created 18/11/2022
+ * @desc To keep things modular we abstract analytic stuff so we can upgrade or change
+ *
+ */
+
+export { buttonAnalytics };
+
+/**
+ *
+ * @author Karl-Johan Bailey
+ * @created 18/11/2022
+ * @desc All button clicks will be processed here
+ * @param string name, object {}
+ *
+ */
+
+// TODO Connect GoogleAnalytics
+
+function buttonAnalytics(name, {}) {}
