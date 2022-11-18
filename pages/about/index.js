9efaff09bc224db5b60106c74a881859841b146b
karl-maker <karljohanbailey98@gmail.com>
2022-11-18 13:33:18 -0400

    Home and About Page
    Added skeleton for pages with description and metat tag for description

---
+++ pages/about/index.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+
+/**
+ *
+ * @author Karl-Johan Bailey
+ * @created 18/11/2022
+ * @desc Intro page explain more about what we do and who we are
+ *
+ */
+
+export default function About() {
+  return (
+    <div>
+      <Head>
+        <title>Dripplie | About</title>
+        <meta
+          name="description"
+          content="Learn about dripplie and how we can help you find any events near you."
+        />
+      </Head>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,23 @@
+import Head from "next/head";
+
+/**
+ *
+ * @author Karl-Johan Bailey
+ * @created 18/11/2022
+ * @desc Intro page to quickly explain what Dripplie is then allow them the option to load events close by with a "Look Around" button
+ *
+ */
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Head>
+        <title>Dripplie</title>
+        <meta
+          name="description"
+          content="Find events near you and experience the best things there are to see, do and eat. Discover events happening near you. Find out about upcoming events and festivals with Dripplie."
+        />
+      </Head>
+    </div>
+  );
 }
