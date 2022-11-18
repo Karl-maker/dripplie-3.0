0109fd1c131e1e088b0255ca51cb8a5586dde50a
karl-maker <karljohanbailey98@gmail.com>
2022-11-18 11:32:50 -0400

    MUI Material Setup
    Added MUI material to project with meta tag for mobile zoom/scale support

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,23 @@
+/**
+ *
+ * @author Karl-Johan Bailey
+ * @company Dripplie.com
+ * @created 18/11/2022
+ * @version 3.0
+ *
+ */
+
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
