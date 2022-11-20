fbd83fda82f7d61ee8586c3788af42e78e9a58ae
karl-maker <karljohanbailey98@gmail.com>
2022-11-20 00:44:28 -0400

    WidgetSkeleton
    For loading widgets / posts / event etc..

---
+++ components/widget/skeleton.js
@@ -0,0 +1,26 @@
+/**
+ *
+ * @author Karl-Johan Bailey
+ * @created 20/11/2022
+ * @desc Loading component for UI's widget
+ *
+ */
+
+import { Skeleton } from "@mui/material";
+
+import defaults from "./defaults";
+
+export default function WidgetSkeleton({
+  height = defaults.height,
+  width = defaults.width,
+}) {
+  const borderRadius = defaults.borderRadius;
+  return (
+    <Skeleton
+      variant="rounded"
+      width={width}
+      height={height}
+      sx={{ bgcolor: "background.default", zIndex: -1, borderRadius }}
+    />
+  );
+}
