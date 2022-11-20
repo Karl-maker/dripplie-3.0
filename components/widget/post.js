bfadbd63ec5946c6ecfa4dd0fdf6a2ff265fe959
karl-maker <karljohanbailey98@gmail.com>
2022-11-20 10:13:23 -0400

    Post Widget Update
    Added profile information to media and content

--- components/widget/post.js
+++ components/widget/post.js
@@ -1,25 +1,48 @@
-import { Box } from "@mui/material";
+/**
+ *
+ * @author Karl-Johan Bailey
+ * @created 20/11/2022
+ * @desc Post widget for showing information
+ *
+ */
+
+import { Box, Typography, Chip, Avatar } from "@mui/material";
 import Image from "next/image";
 
+import UserInfo from "../user/user-info";
 import Widget from ".";
 import defaults from "./defaults";
 
 export default function Post({
   width = defaults.width,
   height = defaults.height,
-  info,
+  author = "",
+  profile_img = "",
+  verified = false,
+  media,
+  text = "",
 }) {
   const padding = "10px";
   const { borderRadius } = defaults;
   const MediaDisplay = ({ src }) => {
     return (
-      <img
-        src={src}
-        alt="Picture of the author"
-        width={width}
-        height={height}
-        objectFit="fill"
-      />
+      <Box sx={{ position: "relative" }}>
+        <Box sx={{ position: "absolute", padding: 1 }}>
+          <UserInfo
+            user={author}
+            profile_img={profile_img}
+            size="small"
+            verified={verified}
+          />
+        </Box>
+        <img
+          src={src}
+          alt="post image"
+          width={width}
+          height={height}
+          objectFit="fill"
+        />
+      </Box>
     );
   };
 
@@ -27,11 +50,7 @@ export default function Post({
     <Widget
       width={width}
       height={height}
-      media={
-        <MediaDisplay
-          src={`https://picsum.photos/${width * 2}/${height * 2}`}
-        />
-      }
+      media={media ? <MediaDisplay src={media.url} /> : null}
     >
       <Box
         height={height}
@@ -42,7 +61,10 @@ export default function Post({
           bgcolor: "background.default",
         }}
       >
-        hi
+        {
+          // Author information
+        }
+        <UserInfo user={author} profile_img={profile_img} verified={verified} />
       </Box>
     </Widget>
   );
