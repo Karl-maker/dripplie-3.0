79d06b45d585133ed1045f186e44414695fa4f57
karl-maker <karljohanbailey98@gmail.com>
2022-11-20 15:07:22 -0400

    Page Names
    Singluar Point of reference for page names

---
+++ constants/pages.js
@@ -0,0 +1,14 @@
+/**
+ * @author Karl-Johan Bailey
+ * @created 20/11/2022
+ * @desc Keeps page names
+ */
+
+const PAGES = {
+  HOME: "/",
+  POST: "/post",
+  ABOUT: "/about",
+  PROFILE: "/profile",
+};
+
+export default PAGES;
