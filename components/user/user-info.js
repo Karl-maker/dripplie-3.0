fabd0481e6af8e0abb17db9d737c37843c363126
karl-maker <karljohanbailey98@gmail.com>
2022-11-20 10:11:38 -0400

    Verification In User Info
    Shows if user is verified

--- components/user/user-info.js
+++ components/user/user-info.js
@@ -6,14 +6,23 @@
  *
  */
 
-import { Typography, Chip, Avatar } from "@mui/material";
+import { Typography, Chip, Avatar, Box } from "@mui/material";
+import { GoVerified } from "react-icons/go";
 
-const UserInfo = ({ size = "normal", user, profile_img }) => {
+const UserInfo = ({ size = "normal", user, profile_img, verified }) => {
+  const marginRightName = 0.5;
   return (
     <Chip
       size={size}
-      label={<Typography variant="caption">{user}</Typography>}
-      avatar={<Avatar src={profile_img || null} alt={user} />}
+      label={
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <Typography variant="caption" sx={{ marginRight: marginRightName }}>
+            {user}
+          </Typography>
+          <GoVerified color="#0984e3" />
+        </Box>
+      }
+      avatar={<Avatar src={profile_img || ""} alt={user} />}
       sx={{ bgcolor: "transparent" }}
     />
   );
