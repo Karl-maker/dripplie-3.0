9c9e862a5dac44e0facdf57cd862709c8692c123
karl-maker <karljohanbailey98@gmail.com>
2022-11-22 02:51:13 -0400

    Layout Update
    Layout setup for UI design

--- components/layout/index.js
+++ components/layout/index.js
@@ -6,10 +6,90 @@
  *
  */
 
+import { Container, Box, Tabs, Tab, Button, Typography } from "@mui/material";
+import { RiLayout4Fill, RiCalendarFill, RiEarthFill } from "react-icons/ri";
+import { useState } from "react";
+
+import language from "../../constants/language";
+
 export default function Layout({ children }) {
+  const [tabValue, setTabValue] = useState(0);
+  const shadow = "0px 5px 30px 1px rgba(10,10,10,0.1)";
+  const NavStyle = {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    bgcolor: "background.secondary",
+    boxShadow: shadow,
+    webkitBoxShadow: shadow,
+    MozBoxShadow: shadow,
+    zIndex: 2,
+    width: "100%",
+  };
+
+  const ICON_SIZE = 25;
+
+  const NavigationList = [
+    {
+      label: "Home",
+      icon: <RiLayout4Fill size={ICON_SIZE} />,
+      link: "",
+    },
+    {
+      label: "Events",
+      icon: <RiCalendarFill size={ICON_SIZE} />,
+      link: "",
+    },
+    {
+      label: "Global",
+      icon: <RiEarthFill size={ICON_SIZE} />,
+      link: "",
+    },
+  ];
+
+  const handleTabChange = (event, newValue) => {
+    setTabValue(newValue);
+  };
+
   return (
-    <>
-      <main>{children}</main>
-    </>
+    <Box sx={{ bgcolor: "background.default", height: "100vh" }}>
+      <Box component="nav" sx={NavStyle} position="fixed">
+        <Box style={{ display: "flex" }}>
+          {/* Logo */}
+          <Box style={{ marginLeft: "20px" }}>
+            <Typography color="text.primary">Dripplie</Typography>
+          </Box>
+          {/* Search Bar */}
+        </Box>
+        {/* Nav Options */}
+        <Tabs
+          direction="row"
+          spacing={2}
+          value={tabValue}
+          onChange={handleTabChange}
+          textColor="primary"
+          indicatorColor="primary"
+        >
+          {NavigationList.map((item, index) => {
+            return <Tab label={item.icon} key={index} sx={{ padding: 2 }} />;
+          })}
+        </Tabs>
+        <Box sx={{ marginRight: "20px" }}>
+          <Button
+            variant="contained"
+            disableElevation
+            sx={{
+              textTransform: "none",
+              width: 80,
+              borderRadius: "5px",
+              color: "#ffff",
+            }}
+          >
+            {language["english"].sign_in}
+          </Button>
+        </Box>
+      </Box>
+      <main style={{ paddingTop: "50px" }}>{children}</main>
+    </Box>
   );
 }
